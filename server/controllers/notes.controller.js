320db380a82f8fe2901b8051499780186a9b6feb
Akriti Srivastava <akisrivastava22@gmail.com>
2023-06-21 16:30:46 +0530

    Update and Delete controller added

--- server/controllers/notes.controller.js
+++ server/controllers/notes.controller.js
@@ -40,6 +40,45 @@ export const createNotes = async(req,res) => {
     }   
 }
 
-export const deleteAllNotes = async(req,res) => {
 
+export const updateNotes = async(req,res) => {
+    try {
+    const {title, description} = req.body
+    const {id: notesId} = req.params
+    let updatedNotes = await Notes.findByIdAndUpdate(notesId, {
+        title,
+        description
+    },{
+        new:true,
+        runValidators: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "Notes Updated",
+        updatedNotes
+    })
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            message: error.message
+        })
+    }
+}
+
+export const deleteAllNotes = async(req,res) => {
+    try {
+    const {id: notesId} = req.params
+    let noteToDelete = await Notes.findById(notesId)
+    noteToDelete.remove();
+
+    res.status(200).json({
+        success: true,
+        message: "Note deleted successfully"
+    })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
