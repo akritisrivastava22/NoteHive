b23dd849324e062e784cd939f507acbd5b2f786a
Akriti Srivastava <akisrivastava22@gmail.com>
2023-06-29 00:01:51 +0530

    functionality addeD

--- client/src/newNote.js
+++ client/src/newNote.js
@@ -1,13 +1,30 @@
-import React from "react"
-
-const newNote = () => {
+import React, {useState} from "react"
+import axios from "axios"
+const NewNote = () => {
+    const [inputText, setInputText] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+      
+        try {
+          const response = await axios.post('/api/notes', {
+            text: inputText,
+          });
+      
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };      
     return(
         <>
-        <button> Go To Notes </button> <br />
-        <input type="text" placeholder="Please enter your new Note" /> <br/>
+
+        {/* <button> Go To Notes </button> <br /> */}
+        <form onSubmit={handleSubmit}>
+        <input type="text" value={inputText} placeholder="Please enter your new Note"  onChange={(e) => setInputText(e.target.value)}  className="bg-black" /> <br/>
         <button> ADD NOTE</button>
+        </form>
         </>
     )
 }
-export default newNote;
+export default NewNote;
 
