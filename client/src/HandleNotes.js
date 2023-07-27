17733825c442dfd746b7cb44c097e502cebf31a8
Akriti Srivastava <akisrivastava22@gmail.com>
2023-07-27 22:55:43 +0530

    imrpoving notes UI

--- client/src/HandleNotes.js
+++ client/src/HandleNotes.js
@@ -54,9 +54,9 @@ const HandleNotes = ({ notes, getAllNotes }) => {
 
   return (
     <>
-      <hr class="h-px bg-gray-200 border-0 mt-3 dark:bg-gray-700" />
+      <hr class="h-px text-cyan-700	 border-0 mt-3 dark:bg-gray-700" />
       <h1 className="font-serif text-xl text-center pt-5 pb-4">ALL NOTES</h1>
-      <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700" />
+      <hr class="h-px text-cyan-700	 border-0 dark:bg-gray-700" />
       {notes.length === 0 ? (
         <h1>No Notes found</h1>
       ) : (
@@ -70,22 +70,22 @@ const HandleNotes = ({ notes, getAllNotes }) => {
                       type="text"
                       value={updatedTitle}
                       onChange={(e) => setUpdatedTitle(e.target.value)}
-                      className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      className="flex h-10 w-full rounded-md border border-amber-50	border-2 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     />
                     <textarea
                       value={updatedDescription}
                       onChange={(e) => setUpdatedDescription(e.target.value)}
-                      className="flex w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      className="flex w-full rounded-md border border-amber-50	border-2 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     ></textarea>
                     <div className="text-center mt-3">
                       <button
-                        className="rounded-md bg-black px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                        className="rounded-md bg-zinc-400	text-zinc-700	font-bold	 px-5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                         onClick={() => handleSaveEdit(note._id)}
                       >
                         Save
                       </button>
                       <button
-                        className="rounded-md ml-2 bg-black px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black	"
+                        className="rounded-md ml-2 bg-zinc-400 text-zinc-700	font-bold px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black	"
                         onClick={handleCancelEdit}
                       >
                         Cancel
@@ -97,18 +97,18 @@ const HandleNotes = ({ notes, getAllNotes }) => {
                 <>
                 {/* <div className="grid grid-cols-2 gap-1 mt-3 w-full"> */}
                   <div className="px-8">
-                <div className="mt-3  w-full rounded-md border border-black/30 bg-transparent px-2  py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50">
-                  <h3 className="">{note.title}</h3>
-                  <p>{note.description}</p>
+                <div className="mt-3  w-full rounded-md border border-amber-50	border-2 bg-transparent px-2  py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50">
+                  <h3 className="text-xl font-semibold">{note.title}</h3>
+                  <p className="text-lg">{note.description}</p>
                   
                   <button
-                    className="mt-2 rounded-md bg-black px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    className="mt-2 rounded-md bg-zinc-400	text-zinc-700	font-bold px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     onClick={() => handleEdit(note._id)}
                   >
                     Edit
                   </button>
                   <button
-                    className="rounded-md ml-2 mt-2 bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    className="rounded-md ml-2 mt-2 bg-zinc-400	text-zinc-700 font-bold	 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     onClick={() => handleDelete(note._id)}
                   >
                     Delete
--- client/src/newNote.js
+++ client/src/newNote.js
@@ -30,8 +30,8 @@ const NewNote = (props) => {
         <>
         {/* <button> Go To Notes </button> <br /> */}
         <form onSubmit={handleSubmit} className="mx-5 mt-3">
-        <input type="text" value={inputTitle} placeholder="Please enter your Notes Title"  onChange={(e) => setInputTitle(e.target.value)}  className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" /> <br/> 
-        <textarea name="message" value={inputdescription} placeholder="Please enter your Notes Description"  onChange={(e) => setInputDescription(e.target.value)} rows="4" cols="50" className="flex w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"></textarea> <br/>
+        <input type="text" value={inputTitle} placeholder="Please enter your Notes Title"  onChange={(e) => setInputTitle(e.target.value)}  className="flex h-10 w-full placeholder:text-white rounded-md border border-amber-50	border-2 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" /> <br/> 
+        <textarea name="message" value={inputdescription} placeholder="Please enter your Notes Description"  onChange={(e) => setInputDescription(e.target.value)} rows="4" cols="50" className="flex placeholder:text-white w-full rounded-md border border-amber-50	border-2 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"></textarea> <br/>
         <button className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black block mx-auto"> ADD NOTE</button>
         </form>
 
